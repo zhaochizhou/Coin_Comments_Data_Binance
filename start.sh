617d2bb1d8d09b57f0256352f9f1b0a69fad80c7
ZhaoChiZhou <zhaochizhou@gmail.com>
2024-06-18 12:05:43 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ now=$year-$month-$day-$hour
 
 
 git config --global user.email ""
-git config --global user.name "coin_spider"
+git config --global user.name "actioner"
 
 git add .
 git commit -m "$now"
