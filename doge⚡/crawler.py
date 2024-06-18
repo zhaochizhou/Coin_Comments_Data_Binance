dceabe6e355431546bc3154083ccdb0568cc0f9d
Hongrui Zhao <62530500+zeturn@users.noreply.github.com>
2024-06-18 16:03:49 +0800

    Update crawler.py

--- doge⚡/crawler.py
+++ doge⚡/crawler.py
@@ -6,6 +6,8 @@ import urllib.parse
 
 def save_data_to_file(data, file_name):
     try:
+        # 确保目录存在
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
         with open(file_name, 'w', encoding='utf-8') as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
         print(f"Data saved to {file_name}")
@@ -18,8 +20,13 @@ os.makedirs('data', exist_ok=True)
 # 定义基础URL
 base_url = "https://www.binance.com/bapi/composite/v2/friendly/pgc/content/queryByHashtag"
 hashtag = "%23doge%E2%9A%A1"
-tag_name = hashtag[3:]  # 去掉开头的#
-encoded_hashtag = urllib.parse.quote(hashtag)
+
+# 先解码 变人话
+decoded_hashtag = urllib.parse.unquote(hashtag)
+
+# 去掉开头的# 这里是人话
+tag_name = decoded_hashtag[1:]
+
 params_template = "?hashtag={}&pageIndex={}&pageSize=20&orderBy=LATEST"
 
 # 定义请求头
@@ -37,24 +44,30 @@ response_data = {
     "code": "000000",
     "message": None,
     "messageDetail": None,
-    "data": []
+    "data": {
+        "hashtag": None,
+        "feedData": []
+    }
 }
 
 # 遍历页数
 for page_index in range(1, 3):
-    url = base_url + params_template.format(encoded_hashtag, page_index)
+    url = base_url + params_template.format(hashtag, page_index)
     try:
         response = requests.get(url, headers=headers)
         response.raise_for_status()  # 抛出异常，如果响应状态码不为200
         data = response.json()
+        
+        # 将hashtag信息保存到response_data
+        if page_index == 1:
+            response_data["data"]["hashtag"] = data.get("data", {}).get("hashtag", {})
+
         # 获取feedData
         feed_data = data.get("data", {}).get("feedData", [])
-        # 如果是第二个页数，将其内容与第一个页数的内容拼接
-        if page_index == 2:
-            response_data["data"].extend(feed_data)
-        else:
-            # 如果是第一个页数，直接添加到response_data中
-            response_data["data"] += feed_data
+        
+        # 将feedData内容添加到response_data中
+        response_data["data"]["feedData"].extend(feed_data)
+
     except requests.RequestException as e:
         print(f"Failed to retrieve data from page {page_index}: {e}")
         # 更新错误信息
@@ -66,8 +79,7 @@ for page_index in range(1, 3):
 current_time_str = datetime.now().strftime("%Y%m%d%H%M%S")
 
 # 生成文件名
-file_name = f"/{tag_name}/data/catch_{current_time_str}.json"
+file_name = f"data/{tag_name}/catch_{current_time_str}.json"
 
 # 保存到文件
 save_data_to_file(response_data, file_name)
-
